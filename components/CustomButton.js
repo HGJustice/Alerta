c5164f2db8a7cdb866ae1eb4722d730e5617168c
HGJustice <125309498+HGJustice@users.noreply.github.com>
2024-03-09 23:01:50 +0100

    second or third commit

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,11 +1,29 @@
 import React from "react";
 import { Text, TouchableOpacity, StyleSheet, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
-const CustomButton = ({ title, onPress, style, textStyle }) => {
+const CustomButton = ({
+  title,
+  onPress,
+  style,
+  textStyle,
+  iconName,
+  iconSize = 24,
+  iconColor = "black",
+}) => {
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={onPress} style={[styles.button, style]}>
-        <Text style={[styles.text, textStyle]}>{title}</Text>
+        <View style={styles.buttonContent}>
+          {iconName && (
+            <FontAwesome name={iconName} size={iconSize} color={iconColor} />
+          )}
+          <Text
+            style={[styles.text, textStyle, iconName && styles.textWithIcon]}
+          >
+            {title}
+          </Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
@@ -17,6 +35,15 @@ const styles = StyleSheet.create({
   },
   button: {
     alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text: {},
+  textWithIcon: {
+    flexDirection: "row",
   },
 });
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,22 +2,35 @@
 import React from "react";
 import { StatusBar, StyleSheet, Text, View } from "react-native";
 import CustomButton from "../components/CustomButton";
-import CustomInput from "../components/CustomInput";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text> Zaloguj sie</Text>
-      <CustomButton />
-      <CustomButton
-        title={"Zaloguj sie za pomoca Google"}
-        style={styles.HomeScreen__button}
-      />
-      <CustomButton
-        title={"Uzyj adresu e-mail"}
-        style={styles.HomeScreen__button}
-      />
-      <StatusBar style="auto" />
+      <View style={styles.HomeScreen__logoContainer}>
+        <Text style={styles.HomeScreen__title}>Logo</Text>
+      </View>
+
+      <View style={styles.HomeScreen__content}>
+        <Text style={styles.HomeScreen__subtitle}> Zaloguj sie </Text>
+        <CustomButton
+          title={"Zaloguj sie za pomoca Google"}
+          style={styles.HomeScreen__button}
+          iconName={"google"}
+          onPress={() => navigation.navigate("Dashboard")}
+        />
+        <CustomButton
+          title={"Uzyj adresu e-mail"}
+          style={styles.HomeScreen__button}
+          iconName={"at"}
+          iconSize={25}
+        />
+        <Text>
+          Nie Masz Konta?{" "}
+          <Text style={{ fontWeight: "bold" }}>Zarejestruj sie</Text>
+        </Text>
+        <CustomButton title={"Nie pamietasz hasla?"} />
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 };
@@ -30,31 +43,36 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     position: "relative",
   },
-  app__header: {
-    fontSize: 40,
-    position: "absolute",
-    top: 200,
-    left: 0,
-    right: 0,
-    textAlign: "center",
-  },
-  loginStyle: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 30,
-    width: 200,
-    textAlign: "center",
-  },
   HomeScreen__button: {
     borderWidth: 1,
     padding: 10,
     height: 62,
     width: 311,
-    borderRadius: 30,
+    borderRadius: 15,
     textAlign: "center",
     justifyContent: "center",
+  },
+  HomeScreen__title: {
+    fontSize: 70,
+  },
+  HomeScreen__logoContainer: {
+    width: 211,
+    height: 211,
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 70,
+    backgroundColor: "#D9D9D9",
+    position: "relative",
+    bottom: 50,
+  },
+  HomeScreen__content: {
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+    top: 25,
+  },
+  HomeScreen__subtitle: {
+    fontSize: 20,
   },
 });
 
