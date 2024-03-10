c64f88311f503cfa8a26f016a85fd44da764c790
Zaluskyy <70617924+Zaluskyy@users.noreply.github.com>
2024-03-10 12:21:05 +0100

    add dangerous and safe mode

--- components/1_Molecues/UserInfo1.js
+++ components/1_Molecues/UserInfo1.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 // import CustomButton from "../0_Atoms/Buttons/CustomButton";
 // import katarinaImg from "../assets/kararinaBlyat.jpg";
@@ -7,11 +7,46 @@ import katarinaImg from "../../assets/kararinaBlyat.jpg";
 export default function UserInfo1() {
   const COLOR = {
     red: "#DC1A1A",
+    blue: "#1AADDC",
   };
   const btnHeight = 70;
   const borderWidth = 5;
 
   const helpBtnArr = ["BLISCY", "OTOCZENIE"];
+
+  const [biometricDataArr, setBiometricDataArr] = useState([
+    { name: "Tętno", result: 82 },
+    { name: "Ciśnienie", result: 120 },
+  ]);
+
+  const [currentColor, setCurrentColor] = useState("#DC1A1A");
+  const [dangerousMode, setDangerousMode] = useState(false);
+
+  useEffect(() => {
+    if (dangerousMode) {
+      setCurrentColor(COLOR.red);
+      setBiometricDataArr([
+        { name: "Tętno", result: 190 },
+        { name: "Ciśnienie", result: 180 },
+      ]);
+    } else {
+      setBiometricDataArr([
+        { name: "Tętno", result: 82 },
+        { name: "Ciśnienie", result: 120 },
+      ]);
+      setCurrentColor(COLOR.blue);
+    }
+  }, [dangerousMode]);
+
+  useEffect(() => {
+    let timerId;
+
+    timerId = setTimeout(() => {
+      setDangerousMode(true);
+    }, 5000);
+
+    return () => clearTimeout(timerId);
+  }, []);
 
   const helpBtn = helpBtnArr.map((item) => {
     return (
@@ -20,24 +55,19 @@ export default function UserInfo1() {
         style={{
           height: btnHeight,
           borderWidth,
-          borderColor: COLOR.red,
+          borderColor: currentColor,
           justifyContent: "center",
           alignItems: "center",
           width: "45%",
           borderRadius: 8,
         }}
       >
-        <Text style={{ fontSize: 20, color: COLOR.red }}>{item}</Text>
+        <Text style={{ fontSize: 20, color: currentColor }}>{item}</Text>
       </TouchableOpacity>
     );
   });
 
-  const biometricDataArr = [
-    { name: "Tętno", result: 190 },
-    { name: "Ciśnienie", result: 180 },
-  ];
-
-  const seconds = 30;
+  const seconds = 10;
 
   const biometricData = biometricDataArr.map((item, index) => {
     return (
@@ -59,7 +89,7 @@ export default function UserInfo1() {
         }}
       >
         <Text style={{ fontSize: 12 }}>{item.name}</Text>
-        <Text style={{ fontSize: 18, color: COLOR.red }}>
+        <Text style={{ fontSize: 18, color: currentColor }}>
           {item.result}
           {index == 0 && " bmp"}
         </Text>
@@ -76,84 +106,101 @@ export default function UserInfo1() {
           </Text>
           <Text style={{ fontSize: 14 }}>24 lat</Text>
         </View>
-        <View style={styles.right}>
+        <View
+          style={{
+            width: 66,
+            height: 66,
+            backgroundColor: currentColor,
+            borderRadius: "50%",
+            overflow: "hidden",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Image style={styles.img} source={katarinaImg} />
         </View>
       </View>
 
       <View style={{ flexDirection: "row", gap: 20 }}>{biometricData}</View>
+      {dangerousMode && (
+        <>
+          <TouchableOpacity
+            style={{
+              width: "100%",
+              height: btnHeight,
+              // backgroundColor: "#ff0000",
+              justifyContent: "center",
+              alignItems: "center",
+              borderWidth: borderWidth,
+              borderColor: currentColor,
+              borderRadius: 8,
+            }}
+          >
+            <Text
+              style={{ fontSize: 20, fontWeight: "bold", color: COLOR.red }}
+            >
+              {seconds > 0 ? `${seconds}s ANULUJ WEZWANIE` : "Wezwano pomoc"}
+            </Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity
-        style={{
-          width: "100%",
-          height: btnHeight,
-          // backgroundColor: "#ff0000",
-          justifyContent: "center",
-          alignItems: "center",
-          borderWidth: borderWidth,
-          borderColor: COLOR.red,
-          borderRadius: 8,
-        }}
-      >
-        <Text style={{ fontSize: 20, fontWeight: "bold", color: COLOR.red }}>
-          {seconds > 0 ? `${seconds}s ANULUJ WEZWANIE` : "Wezwano pomoc"}
-        </Text>
-      </TouchableOpacity>
+          <View
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {helpBtn}
+          </View>
 
-      <View
-        style={{
-          width: "100%",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        {helpBtn}
-      </View>
-
-      <TouchableOpacity
-        style={{
-          width: "100%",
-          padding: 10,
-          // backgroundColor: "rgba(255, 0, 0, 0.3)",
-          position: "relative",
-        }}
-      >
-        <View
-          style={{
-            width: "60%",
-            height: btnHeight * 0.8,
-            backgroundColor: "rgba(255, 0, 0, .3)",
-            position: "absolute",
-            borderRadius: 8,
-          }}
-        />
-        <View
-          style={{
-            width: "60%",
-            height: btnHeight * 0.8,
-            backgroundColor: "rgba(255, 0, 0, .3)",
-            position: "absolute",
-            right: 0,
-            bottom: 0,
-            borderRadius: 8,
-          }}
-        />
-        <View
-          style={{
-            width: "100%",
-            height: btnHeight,
-            backgroundColor: COLOR.red,
-            borderRadius: 15,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text style={{ fontSize: 36, fontWeight: "bold", color: "#ffffff" }}>
-            112
-          </Text>
-        </View>
-      </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: "100%",
+              padding: 10,
+              // backgroundColor: "rgba(255, 0, 0, 0.3)",
+              position: "relative",
+            }}
+          >
+            <View
+              style={{
+                width: "60%",
+                height: btnHeight * 0.8,
+                backgroundColor: "rgba(255, 0, 0, .3)",
+                position: "absolute",
+                borderRadius: 8,
+              }}
+            />
+            <View
+              style={{
+                width: "60%",
+                height: btnHeight * 0.8,
+                backgroundColor: "rgba(255, 0, 0, .3)",
+                position: "absolute",
+                right: 0,
+                bottom: 0,
+                borderRadius: 8,
+              }}
+            />
+            <View
+              style={{
+                width: "100%",
+                height: btnHeight,
+                backgroundColor: currentColor,
+                borderRadius: 15,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{ fontSize: 36, fontWeight: "bold", color: "#ffffff" }}
+              >
+                112
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 }
@@ -183,15 +230,15 @@ const styles = StyleSheet.create({
     alignItems: "flex-end",
     justifyContent: "space-evenly",
   },
-  right: {
-    width: 66,
-    height: 66,
-    backgroundColor: "#DC1A1A",
-    borderRadius: "50%",
-    overflow: "hidden",
-    justifyContent: "center",
-    alignItems: "center",
-  },
+  // right: {
+  // width: 66,
+  // height: 66,
+  // backgroundColor: currentColor,
+  // borderRadius: "50%",
+  // overflow: "hidden",
+  // justifyContent: "center",
+  // alignItems: "center",
+  // },
   img: {
     width: "90%",
     height: "90%",
