e431da2c063987f2f08fd3554f4cfe442e4b2aef
Zaluskyy <70617924+Zaluskyy@users.noreply.github.com>
2024-03-10 12:58:40 +0100

    change layout view2

--- components/1_Molecues/UserInfo2.js
+++ components/1_Molecues/UserInfo2.js
@@ -1,5 +1,14 @@
-import React, {useState} from "react";
-import {Alert, Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import React, { useState } from "react";
+import {
+  Alert,
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import queryOpenAI from "../Apis/openAIQuery";
 import katarinaImg from "../../assets/kararinaBlyat.jpg"; // Asumując, że ścieżka jest poprawna
 
@@ -16,50 +25,50 @@ export default function UserInfo2() {
   const helpBtnArr = ["BLISCY", "OTOCZENIE"];
 
   const biometricDataArr = [
-    { name: "Tętno", result: 190 },
-    { name: "Ciśnienie", result: 180 },
+    { name: "Tętno", result: 20 },
+    { name: "Ciśnienie", result: 70 },
   ];
   const biometricData = biometricDataArr.map((item, index) => {
     return (
-        <View
-            key={item.name}
-            style={{
-              width: 100,
-              height: 50,
-              flexDirection: "column",
-              alignContent: "flex-start",
-              justifyContent: "space-evenly",
-              padding: 8,
+      <View
+        key={item.name}
+        style={{
+          width: 100,
+          height: 50,
+          flexDirection: "column",
+          alignContent: "flex-start",
+          justifyContent: "space-evenly",
+          padding: 8,
 
-              backgroundColor: "#F3F3F3",
-              borderRadius: 10,
-            }}
-        >
-          <Text style={{ fontSize: 12 }}>{item.name}</Text>
-          <Text style={{ fontSize: 18, color: COLOR.red }}>
-            {item.result}
-            {index == 0 && " bmp"}
-          </Text>
-        </View>
+          backgroundColor: "#F3F3F3",
+          borderRadius: 10,
+        }}
+      >
+        <Text style={{ fontSize: 12 }}>{item.name}</Text>
+        <Text style={{ fontSize: 18, color: COLOR.red }}>
+          {item.result}
+          {index == 0 && " bmp"}
+        </Text>
+      </View>
     );
   });
 
   const helpBtn = helpBtnArr.map((item) => {
     return (
-        <TouchableOpacity
-            key={item}
-            style={{
-              height: btnHeight,
-              borderWidth,
-              borderColor: COLOR.red,
-              justifyContent: "center",
-              alignItems: "center",
-              width: "45%",
-              borderRadius: 8,
-            }}
-        >
-          <Text style={{ fontSize: 20, color: COLOR.red }}>{item}</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        key={item}
+        style={{
+          height: btnHeight,
+          borderWidth,
+          borderColor: COLOR.red,
+          justifyContent: "center",
+          alignItems: "center",
+          width: "45%",
+          borderRadius: 8,
+        }}
+      >
+        <Text style={{ fontSize: 20, color: COLOR.red }}>{item}</Text>
+      </TouchableOpacity>
     );
   });
 
@@ -68,22 +77,26 @@ export default function UserInfo2() {
       const response = await queryOpenAI();
       if (response) {
         setOutputText(response);
-        setButtonPressed(true)
+        setButtonPressed(true);
       } else {
-        setOutputText(' 1. Natychmiast zadzwoń na numer alarmowy i poproś o pomoc medyczną.\n' +
-            '2. Sprawdź oddech i puls poszkodowanego.\n' +
-            '3. Rozpocznij resuscytację krążeniowo-oddechową, jeśli nie ma tętna.\n' +
-            '4. Unieś nogi poszkodowanego, aby poprawić krążenie.\n' +
-            '5. Zachowaj spokój i wykonuj czynności ratunkowe do czasu przybycia służb medycznych.\n');
-        setButtonPressed(true)
+        setOutputText(
+          " 1. Natychmiast zadzwoń na numer alarmowy i poproś o pomoc medyczną.\n" +
+            "2. Sprawdź oddech i puls poszkodowanego.\n" +
+            "3. Rozpocznij resuscytację krążeniowo-oddechową, jeśli nie ma tętna.\n" +
+            "4. Unieś nogi poszkodowanego, aby poprawić krążenie.\n" +
+            "5. Zachowaj spokój i wykonuj czynności ratunkowe do czasu przybycia służb medycznych.\n"
+        );
+        setButtonPressed(true);
       }
     } catch (error) {
-      setOutputText(' 1. Natychmiast zadzwoń na numer alarmowy i poproś o pomoc medyczną.\n' +
-          '2. Sprawdź oddech i puls poszkodowanego.\n' +
-          '3. Rozpocznij resuscytację krążeniowo-oddechową, jeśli nie ma tętna.\n' +
-          '4. Unieś nogi poszkodowanego, aby poprawić krążenie.\n' +
-          '5. Zachowaj spokój i wykonuj czynności ratunkowe do czasu przybycia służb medycznych.\n');
-      setButtonPressed(true)
+      setOutputText(
+        " 1. Natychmiast zadzwoń na numer alarmowy i poproś o pomoc medyczną.\n" +
+          "2. Sprawdź oddech i puls poszkodowanego.\n" +
+          "3. Rozpocznij resuscytację krążeniowo-oddechową, jeśli nie ma tętna.\n" +
+          "4. Unieś nogi poszkodowanego, aby poprawić krążenie.\n" +
+          "5. Zachowaj spokój i wykonuj czynności ratunkowe do czasu przybycia służb medycznych.\n"
+      );
+      setButtonPressed(true);
     }
   };
 
@@ -92,79 +105,173 @@ export default function UserInfo2() {
     setButtonPressed(false);
   };
 
+  const largeBtnStyle = {
+    height: btnHeight,
+    borderWidth,
+    borderColor: COLOR.red,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+
+    marginTop: 20,
+    marginBottom: 20,
+    borderRadius: 8,
+  };
+
   return (
-      <View style={buttonPressed ? styles.UserInfoAdvanced : styles.UserInfo}>
-        <View style={styles.personalData}>
-          <View style={styles.left}>
-            <Text style={{ fontSize: 14, fontWeight: "bold" }}>
-              Katarzyna Nowak
-            </Text>
-            <Text style={{ fontSize: 14 }}>24 lat</Text>
-          </View>
-          <View style={styles.right}>
-            <Image style={styles.img} source={katarinaImg} />
-          </View>
+    <View style={buttonPressed ? styles.UserInfoAdvanced : styles.UserInfo}>
+      <View style={styles.personalData}>
+        <View style={styles.left}>
+          <Text style={{ fontSize: 14, fontWeight: "bold" }}>
+            Katarzyna Nowak
+          </Text>
+          <Text style={{ fontSize: 14 }}>24 lat</Text>
         </View>
-
-        <View style={{ flexDirection: "row", gap: 20 }}>{biometricData}</View>
-        {!buttonPressed ? (
-                <TouchableOpacity
-                    style={{
-                      height: 70,
-                      borderWidth,
-                      borderColor: COLOR.red,
-                      justifyContent: "center",
-                      alignItems: "center",
-                      width: "45%",
-                      borderRadius: 8,
-                      marginTop: 10
-                    }}
-                >
-                  <Button title="Jestem na miejscu" onPress={handleOpenAIQuery} />
-
-                </TouchableOpacity>
-
-        )
-
-            :
-
-            (
-            <>
-              <TouchableOpacity
-                  style={{
-                    height: 70,
-                    borderWidth,
-                    borderColor: COLOR.red,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    width: "45%",
-                    borderRadius: 8,
-                    marginTop: 10
-                  }}
-              >
-                <Button title="Gdzie jest AED" onPress={() => Alert.alert("Najbliższy AED")} />
-              </TouchableOpacity>
-
-              <TextInput
-                  style={styles.largeTextInput}
-                  multiline={true}
-                  numberOfLines={4}
-                  editable={false}
-                  selectTextOnFocus={false}
-                  value={outputText}
-              />
-              <Button title="Resetuj" onPress={resetState} />
-            </>
-        )}
+        <View style={styles.right}>
+          <Image style={styles.img} source={katarinaImg} />
+        </View>
       </View>
+
+      <View style={{ flexDirection: "row", gap: 20 }}>{biometricData}</View>
+      {!buttonPressed ? (
+        <TouchableOpacity
+          onPress={handleOpenAIQuery}
+          style={{
+            height: btnHeight,
+            borderWidth,
+            borderColor: COLOR.red,
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+
+            marginTop: 20,
+            marginBottom: 20,
+            borderRadius: 8,
+            // marginTop: 20,
+            // marginBottom: 20,
+          }}
+        >
+          <Text
+            style={{
+              color: COLOR.red,
+              fontSize: 20,
+              fontWeight: "bold",
+            }}
+          >
+            Jestem na miejscu
+          </Text>
+        </TouchableOpacity>
+      ) : (
+        <>
+          <TouchableOpacity
+            style={largeBtnStyle}
+            onPress={() => Alert.alert("Najbliższy AED")}
+          >
+            <Text
+              style={{
+                color: COLOR.red,
+                fontSize: 20,
+                fontWeight: "bold",
+              }}
+            >
+              Gdzie jest AED
+            </Text>
+            {/* <Button
+              title="Gdzie jest AED"
+              // onPress={() => Alert.alert("Najbliższy AED")}
+            /> */}
+          </TouchableOpacity>
+
+          {/* <TextInput
+            style={styles.largeTextInput}
+            // style={{
+            //   backgroundColor: "#EDEDED",
+            //   padding: 30,
+            //   flexDirection: "column",
+            //   alignItems: "center",
+            // }}
+            multiline={true}
+            numberOfLines={4}
+            editable={false}
+            selectTextOnFocus={false}
+            value={outputText}
+          /> */}
+
+          <View
+            style={{
+              backgroundColor: "#EDEDED",
+              padding: 30,
+              flexDirection: "column",
+              justifyContent: "center",
+              gap: 10,
+              borderRadius: 8,
+            }}
+          >
+            <Text style={{ fontWeight: "bold" }}>ChatGPT</Text>
+            <Text style={{ fontWeight: 500 }}>
+              Wykoanj następujące czynności:
+            </Text>
+            <Text>{outputText}</Text>
+          </View>
+
+          <TouchableOpacity
+            style={{
+              width: "100%",
+              padding: 10,
+              // backgroundColor: "rgba(255, 0, 0, 0.3)",
+              position: "relative",
+              marginTop: 20,
+            }}
+          >
+            <View
+              style={{
+                width: "60%",
+                height: btnHeight * 0.8,
+                backgroundColor: "rgba(255, 0, 0, .3)",
+                position: "absolute",
+                borderRadius: 8,
+              }}
+            />
+            <View
+              style={{
+                width: "60%",
+                height: btnHeight * 0.8,
+                backgroundColor: "rgba(255, 0, 0, .3)",
+                position: "absolute",
+                right: 0,
+                bottom: 0,
+                borderRadius: 8,
+              }}
+            />
+            <View
+              style={{
+                width: "100%",
+                height: btnHeight,
+                backgroundColor: COLOR.red,
+                borderRadius: 15,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{ fontSize: 36, fontWeight: "bold", color: "#ffffff" }}
+              >
+                112
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          {/* <Button title="Resetuj" onPress={resetState} /> */}
+        </>
+      )}
+    </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   UserInfo: {
     zIndex: 1,
-    height: "30%",
+    // height: "30%",
     backgroundColor: "#fff",
     padding: 30,
     width: "100%",
@@ -177,7 +284,7 @@ const styles = StyleSheet.create({
   },
   UserInfoAdvanced: {
     zIndex: 1,
-    height: "70%",
+    // height: "70%",
     backgroundColor: "#fff",
     padding: 30,
     width: "100%",
@@ -200,6 +307,8 @@ const styles = StyleSheet.create({
   personalData: {
     flexDirection: "row",
     alignSelf: "flex-end",
+    marginBottom: 20,
+    marginTop: 20,
     gap: 9,
   },
   left: {
--- components/Apis/openAIQuery.js
+++ components/Apis/openAIQuery.js
@@ -1,28 +1,32 @@
 const queryOpenAI = async () => {
-    try {
-        const response = await fetch('https://api.openai.com/v1/chat/completions', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer sk-HTCzjKXC02Arbrwe6QxdT3BlbkFJ5mnxQGe0ZWm6W6sky7xiks`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                model: 'gpt-3.5-turbo',
-                messages: [
-                    {role: 'user', content: 'Jest osoba, które obniżyło się tętno praktycznie do 0, co robić w takiej sytuacji? Wylistuj mi w punktach zadanie co robić, maksymalnie 350 znaków. Nie dodawaj nic poza punktami.'}
-                ]
-            }),
-        });
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer 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`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content:
+              "Jest osoba, które obniżyło się tętno praktycznie do 0, co robić w takiej sytuacji? Wylistuj mi w punktach zadanie co robić, maksymalnie 350 znaków. Nie dodawaj nic poza punktami.",
+          },
+        ],
+      }),
+    });
 
-        const data = await response.json();
-        // Wyodrębnienie zawartości pierwszej wiadomości z pierwszego wyboru
-        const messageContent = data.choices[0].message.content;
-        console.log(messageContent); // Opcjonalnie: wyświetlenie w konsoli
-        return messageContent; // Zwrócenie zawartości wiadomości
-    } catch (error) {
-        console.error('Error:', error);
-        return null; // lub obsłuż błąd inaczej, zależnie od potrzeb
-    }
+    const data = await response.json();
+    // Wyodrębnienie zawartości pierwszej wiadomości z pierwszego wyboru
+    const messageContent = data.choices[0].message.content;
+    console.log(messageContent); // Opcjonalnie: wyświetlenie w konsoli
+    return messageContent; // Zwrócenie zawartości wiadomości
+  } catch (error) {
+    console.error("Error:", error);
+    return null; // lub obsłuż błąd inaczej, zależnie od potrzeb
+  }
 };
 
 // Eksport funkcji
